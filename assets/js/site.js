f94e9a021a2a444df729c99a6c1664e749f31f00
GBM1986 <gunnar_bjarni@hotmail.com>
2023-09-07 16:37:04 +0200

    FetchfoodpartlyDone

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,54 +1,107 @@
-// serch mode to determine serch button clicked
-let serchMode = "none";
+// search mode to determine search button clicked
+let searchMode = "none";
 
-// dom elements for function --------------------------------------------------------
-
+// DOM elements for functions --------------------------------------------------------
 const myResultElement = document.getElementById("myResult");
 
-const myfirstLetterInput = document.getElementById("firstLetterInput");
-const myfirstLetterSearchButton = document.getElementById("firstLetterSearch");
+const myFirstLetterInput = document.getElementById("firstLetterInput");
+const myFirstLetterSearchButton = document.getElementById("firstLetterSearch");
 
-myfirstLetterSearchButton.addEventListener("click", () => {
-  serchMode = "firstLetterSearch";
-  console.info(myfirstLetterInput.value);
+myFirstLetterSearchButton.addEventListener("click", () => {
+  searchMode = "firstLetterSearch";
+  console.info(myFirstLetterInput.value);
+  getRecipesByFirstLetter(); // Call the function without passing any arguments
 });
 
 const myNameInput = document.getElementById("nameInput");
 const myNameSearchButton = document.getElementById("nameSearch");
 
 myNameSearchButton.addEventListener("click", () => {
-  serchMode = "nameSearch";
+  searchMode = "nameSearch";
   console.info(myNameInput.value);
-  getRecipiesByName(myNameInput.value);
+  getRecipesByName(myNameInput.value);
 });
 
 const myIdInput = document.getElementById("idInput");
 const myIdSearchButton = document.getElementById("idSearch");
 
 myIdSearchButton.addEventListener("click", () => {
-  serchMode = "idSearch";
+  searchMode = "idSearch";
   console.info(myIdInput.value);
+  getRecipesById(myIdInput.value); // Pass the input value as an argument
 });
 
 //-------------------------------------------------------------------------------------
 
-// fetch functions --------------------------------------------------------------------
-// your code goes here
+// Fetch functions --------------------------------------------------------------------
 
+// Fiken hjelp fra chatgpt ikke helt sikkert at jeg forstar det hille
 
+function getRecipesByFirstLetter() {
+  const firstLetter = myFirstLetterInput.value; // Access the value of the input field
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`;
 
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok (${response.status})`);
+      }
+      return response.json();
+    })
+    .then((myData) => {
+      setupResultView(myData);
+    })
+    .catch((error) => {
+      searchMode = "errorMessage";
+      console.error("Error:", error);
+    });
+}
 
+function getRecipesByName(myName) {
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${myName}`;
 
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok (${response.status})`);
+      }
+      return response.json();
+    })
+    .then((myData) => {
+      setupResultView(myData);
+    })
+    .catch((error) => {
+      searchMode = "errorMessage";
+      console.error("Error:", error);
+    });
+}
 
+function getRecipesById(myId) {
+  const apiUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${myId}`;
 
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok (${response.status})`);
+      }
+      return response.json();
+    })
+    .then((myData) => {
+      setupResultView(myData);
+    })
+    .catch((error) => {
+      searchMode = "errorMessage";
+      console.error("Error:", error);
+    });
+}
 
-// view code --------------------------------------------------------------------------
+// View code --------------------------------------------------------------------------
 
 function setupResultView(myData) {
-  switch (serchMode) {
+  switch (searchMode) {
     case "firstLetterSearch":
       console.log(myData);
-      // do view stuff with the data here
+      // Do view stuff with the data here
       break;
 
     case "nameSearch":
@@ -64,16 +117,16 @@ function setupResultView(myData) {
 
     case "idSearch":
       console.log(myData);
-      // do view stuff with the data here
+      // Do view stuff with the data here
       break;
 
     case "errorMessage":
       console.log(myData);
-      // do view stuff with the error msg here
+      // Handle error message
       break;
 
     default:
-      console.warn("ooops no data to show from setupResultView");
+      console.warn("Oops, no data to show from setupResultView");
       break;
   }
 }
